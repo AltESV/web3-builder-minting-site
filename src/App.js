2c87c4497d6a80f7f38a6ee6f466a38cf93e76e8
AltESV <ishigami.daniel@gmail.com>
2022-09-25 20:10:38 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -5,8 +5,13 @@ import nftvideo from "./assets/nftvideo.mp4";
 import StartMinting from './components/StartMinting';
 import InProgressMinting from './components/InProgressMinting';
 import CompletedMinting from './components/CompletedMinting';
+import { ethers } from "ethers";
+import abi from "./contracts/contract.json";
 
 function App() {
+// saves user wallet and supply in session storage 
+  const [supply, setSupply] = useState(0);
+  const [account, setAccount] = useState() 
   const [inProgress, setInProgress] = useState(false);
   const [completed, setCompleted] = useState(false);
 
@@ -19,7 +24,24 @@ function App() {
   }
 
   const login = async () => {
-    
+    // Option from MetaMask docs
+    if (typeof window.ethereum !== 'undefined') {
+      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+      const walletAccount = accounts[0];
+      setAccount(walletAccount);
+      // saved contract address from class connect code here https://ethereum.stackexchange.com/questions/120817/how-to-call-a-contract-function-method-using-ethersjs
+      const contractAddress = "0xa3a3EF770249ADeCE37f3Be3d43BB878Bb57a8E2";
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner(walletAccount);
+      const NFTContract = new ethers.Contract(contractAddress, abi, signer);
+      
+// gives you total supply to feed into mint count on website
+      const name = await NFTContract.name()
+      const totalSupply = await NFTContract.totalSupply()
+      setSupply(totalSupply.toNumber());
+      
+    }
+
   }
 
   const getState = () => { 
@@ -37,6 +59,7 @@ function App() {
   }
 
 
+  //displays html elements 
   return (
     <div className="App">
       {/* hack autoplay only is on if muted also on */}
@@ -54,14 +77,18 @@ function App() {
       
             </div>
             <div class="information">
-              <h2>Web3 Builders into the metaverse</h2>
-              <p>0 Minted / 200</p>
-              <button class="button">CONNECT WALLET</button>
+              <h2>Web3 Builders Into The Metaverse</h2>
+              <p>{supply} Minted / 200 💎</p>
+              {/* shows different button depending on if wallet connected*/}
+              {account ? <button onClick={login} class="button">MINT</button> :
+              <button onClick={login} class="button">CONNECT WALLET</button>
+              }
+    
             </div>
             
           </div>  
           <div class="footer">
-            MINTING NOW
+          🎉 MINTING NOW 🎉
           </div>
         </div>
     </div>
